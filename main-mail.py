5e8988325221aa9e9d808191878fb801b8f488be
easypatents <robiibanez@gmail.com>
2017-06-13 20:37:23 -0400

    Busqueda desde EPO-OPS

--- main-mail.py
+++ main-mail.py
@@ -1,6 +1,8 @@
 from EPmail import EPmail
 from Busquedas import*
 import epo_ops
+from bs4 import BeautifulSoup
+
 
 def main():
 
@@ -35,19 +37,68 @@ def main():
     #print(text)
     #aux = collocationFinder(2,5,text)
 
-    word = "dog"
-    print(get_synonymous(word))
+    #word = "dog"
+    #print(get_synonymous(word))
     #print(get_antonyms(word))
 
-    print(similaridad("dog","frump"))
-    client = epo_ops.Client(key='abc', secret='xyz')  # Instantiate client
-    response = client.published_data(  # Retrieve bibliography data
-        reference_type='publication',  # publication, application, priority
-        input=epo_ops.models.Docdb('1000000', 'EP', 'A1'),  # original, docdb, epodoc
-        endpoint='biblio',  # optional, defaults to biblio in case of published_data
-        constituents=[]  # optional, list of constituents
-    )
+    #print(similaridad("dog","frump"))
 
+    consumer_key = 'a66G2Ox2G6JYLlp9VTQnZ6Dqb7GGtmdn'
+    consumer_secret_key = '6EAoLMWT7gHZBGqy'
+
+    client = epo_ops.Client(key=consumer_key, secret=consumer_secret_key)  # Instantiate client
+
+    response = client.published_data_search(cql='ab=explosive',range_begin=1,range_end=25,constituents=None)
+    #
+    # response = client.published_data(  # Retrieve bibliography data
+    #     reference_type='publication',  # publication, application, priority
+    #     input = epo_ops.models.Docdb('1000000', 'EP','A1'),  # original, docdb, epodoc
+    #     #query='ti%3Dplastic' ,
+    #     endpoint='biblio',  # optional, defaults to biblio in case of published_data
+    #     constituents=[]  # optional, list of constituents
+    # )
+
+    #print(response.text)
+    #print(soup.prettify())
+
+    #count = 1
+    #for aux in soup:
+    #    print(aux)
+    #    if count>3:
+    #        break
+    #    count+=1
+
+    country = busquedaEPO(response,'country',type='html')
+    number = busquedaEPO(response,'doc-number',type='html')
+    kind = busquedaEPO(response, 'kind',type='html')
+    #import epo_opsanonymous_client = epo_ops.Client()  # Instantiate a default client
+
+
+    for i in range(len(country)):
+        response = client.published_data(  # Retrieve bibliography data
+            reference_type='publication',  # publication, application, priority
+            input=epo_ops.models.Docdb(str(number[i]), country[i], kind[i]),  # original, docdb, epodoc
+            endpoint='abstract',
+            # optional, defaults to biblio in case of published_data
+        )
+        #aux = busquedaEPO(response,elemento='abstract',type='xml')
+        #print(getSoup(response,'html.parser').find(lang='en').p.string)
+        print(busquedaLang(response,idioma='en',type='html.parser'))
+        #if len(aux)<=1:
+        #    print(aux[0])
+        #else:
+        #    print(aux[1])
+
+
+
+    #     text = response.text
+    #     soup = BeautifulSoup(text, 'xml')
+    #     aux2 = soup.find_all('abstract')
+    #     #fin = list()
+    #     for i in aux2:
+    #         print(i.p.string)
+        #print(response.text)
+        #print(busquedaEPO(BeautifulSoup(response.text,'html.parser'),'abstract'))
 
 if __name__ == "__main__":
     main()
