9fd5d228a24837665442db1d4ee9061ff84e1ef4
Diego Valenzuela <tachuelaz@gmail.com>
2017-06-15 01:24:53 -0400

    from2data.py - Hasta ahora lee las respuestas desde Typeform y extrae nombre, mail y respuesta usando json

---
+++ form2data.py
@@ -0,0 +1,50 @@
+import json
+import urllib.request
+
+# https://easypatents.typeform.com/to/BdzCUz
+# a4b5f45414ebad1defbb80651a961ef09e364e82
+
+
+def main():
+
+
+    usr = "BdzCUz"
+    key = "a4b5f45414ebad1defbb80651a961ef09e364e82"
+
+    url = "https://api.typeform.com/v1/form/" + usr + "?" + "key=" + key
+    url_specs = "&" + "completed=true" + "&" + "order_by[]=date_submit,desc" # + "&" + "limit=10"
+
+    url = url + url_specs
+
+
+    page = urllib.request.urlopen(url)
+
+    content_bytes = page.read()
+    content = content_bytes.decode('utf8')
+
+    obj = json.loads(content)
+
+    ##print(json.dumps(obj, indent=4)) ### NO BORRAR
+
+
+    completed = obj['stats']['responses']['completed']  # total de respuestas completadas
+    showing = obj['stats']['responses']['showing']      # total de respuestas que son mostradas
+
+    #print(completed)
+    #print(showing)
+
+
+    for i in range(showing):
+
+        respuesta = obj['responses'][i]['answers']['textarea_52850750']
+        email = obj['responses'][i]['answers']['email_52850524']
+        nombre = obj['responses'][i]['answers']['textfield_52850379']
+
+        print('\n')
+        print(nombre)
+        print(email)
+        print(respuesta)
+
+
+if __name__ == "__main__":
+    main()
