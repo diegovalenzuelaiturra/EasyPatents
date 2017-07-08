e136fcfeceddeabf911c3801f5b80e784856b9a8
Diego Valenzuela <tachuelaz@gmail.com>
2017-07-08 03:31:39 -0400

    Ploteando algunas cositas (sin utilidad por ahora)

--- TX_vec_Analysis.py
+++ TX_vec_Analysis.py
@@ -2,6 +2,11 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 
+from sklearn.cluster import DBSCAN
+from sklearn import metrics
+from sklearn.preprocessing import StandardScaler
+import matplotlib.pyplot as plt
+
 
 def main():
 
@@ -33,6 +38,51 @@ def main():
 #   Reconstruction
     USV = np.dot(U, np.dot(S, V))
     # print(USV)
+
+
+########################################################################################################################
+
+# USELESS
+
+#   http://scikit-learn.org/stable/auto_examples/cluster/plot_dbscan.html #sphx-glr-auto-examples-cluster-plot-dbscan-py
+
+#   eps = 5
+#   eps = 25
+
+    X = TX_vec
+    X = StandardScaler().fit_transform(X)
+    db = DBSCAN(eps=15, min_samples=3).fit(X)
+    core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
+    core_samples_mask[db.core_sample_indices_] = True
+    labels = db.labels_
+
+    # Number of clusters in labels, ignoring noise if present.
+    n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
+
+    print('Estimated number of clusters: %d' % n_clusters_)
+    print("Silhouette Coefficient: %0.3f" % metrics.silhouette_score(X, labels))
+
+
+    # Black removed and is used for noise instead.
+    unique_labels = set(labels)
+    colors = [plt.cm.Spectral(each) for each in np.linspace(0, 1, len(unique_labels))]
+    for k, col in zip(unique_labels, colors):
+        if k == -1:
+            # Black used for noise.
+            col = [0, 0, 0, 1]
+
+        class_member_mask = (labels == k)
+
+        xy = X[class_member_mask & core_samples_mask]
+        plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=tuple(col), markeredgecolor='k', markersize=14)
+
+        xy = X[class_member_mask & ~core_samples_mask]
+        plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=tuple(col), markeredgecolor='k', markersize=6)
+
+    plt.title('Estimated number of clusters: %d' % n_clusters_)
+    plt.show()
+
+########################################################################################################################
 
 
 if __name__ == '__main__':
